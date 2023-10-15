dae1dcdc9d2bc2c2667695011f03d083e35a404d
HARDIK-SHARMA-08 <hardiksharma602@gmail.com>
2023-10-15 16:10:07 +0530

    Changes

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -4,7 +4,7 @@ import Banner1 from "../Assets/hero.jpeg";
 const HeroSection = () => {
   return (
     <>
-      <div className="w-full mt-0 h-72 overflow-hidden rounded-b-3xl md:h-3/4">
+      <div className="w-full mt-0 h-96 overflow-hidden rounded-b-3xl md:h-3/4">
         <div className="duration-700 ease-in-out">
           <img src={Banner1} className="w-full h-full object-cover md:h-fit" alt="Hero Image" />
         </div>

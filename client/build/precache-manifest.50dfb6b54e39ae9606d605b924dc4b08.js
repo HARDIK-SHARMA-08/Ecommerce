df9effdd48a5036d618ad5bd3f7219f25cdc9351
HARDIK-SHARMA-08 <hardiksharma602@gmail.com>
2023-10-15 15:46:37 +0530

    Final Changes

---
+++ client/build/precache-manifest.50dfb6b54e39ae9606d605b924dc4b08.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4988c038cd683f112bf3",
+    "url": "/static/css/main.40bcb6df.chunk.css"
+  },
+  {
+    "revision": "4988c038cd683f112bf3",
+    "url": "/static/js/main.4988c038.chunk.js"
+  },
+  {
+    "revision": "aae0894a59744fd6fbf0",
+    "url": "/static/js/1.aae0894a.chunk.js"
+  },
+  {
+    "revision": "60486c7a81c014bd4084",
+    "url": "/static/css/2.9d23a6b3.chunk.css"
+  },
+  {
+    "revision": "60486c7a81c014bd4084",
+    "url": "/static/js/2.60486c7a.chunk.js"
+  },
+  {
+    "revision": "ff0abb504a9f5525fcae",
+    "url": "/static/js/runtime~main.ff0abb50.js"
+  },
+  {
+    "revision": "d1b6dae022485be88ba8cdc3bbfcb109",
+    "url": "/static/media/hero.d1b6dae0.jpeg"
+  },
+  {
+    "revision": "c34a1ddef3b7f8672b6c9b37c115bf0e",
+    "url": "/static/media/Gojo_Back_2-removebg-preview.c34a1dde.png"
+  },
+  {
+    "revision": "b017e2f5814d88777d6f3d3a8e10e50f",
+    "url": "/static/media/0526420730_6_1_1-removebg-preview.b017e2f5.png"
+  },
+  {
+    "revision": "161559cb30bd3a520c92bc907cfd3e95",
+    "url": "/static/media/0108401707_6_1_1-removebg-preview.161559cb.png"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "0dc96a71e56d4544d23cfd8cfed3e0a6",
+    "url": "/index.html"
+  }
+];
